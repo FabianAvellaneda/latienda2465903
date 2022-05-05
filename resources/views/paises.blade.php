264215ac7580f620bb63ac4a7f30128c8d6edb65
Fabian <you@example.com>
2022-05-05 14:51:14 -0500

    evidencia 1

--- resources/views/paises.blade.php
+++ resources/views/paises.blade.php
@@ -22,19 +22,24 @@
             <tbody>
                 @foreach($paises as $pais => $infopais)
                 <tr>
-                    <td>
+                    <td rowspan='{{ count($infopais["ciudades"]) }}'>
                         {{ $pais }}
                     </td>
-                    <td>
+                    <td rowspan='{{ count($infopais["ciudades"]) }}'>
                         {{ $infopais["capital"] }}
                     </td>
-                    <td>
+                    <td rowspan='{{ count($infopais["ciudades"]) }}'>
                         {{ $infopais["moneda"] }}
                     </td>
-                    <td>
-                        {{ $infopais["poblacion"] }}
+                    <td rowspan='{{ count($infopais["ciudades"]) }}'>
+                        {{ $infopais["poblacion"] }} millones hab.
                     </td>
+                    @foreach($infopais["ciudades"] as $ciudad)
+                    <th>
+                        {{ $ciudad }} 
+                    </th>
                 </tr>
+                    @endforeach
                 @endforeach
             </tbody>
             <tfooter></tfooter>
